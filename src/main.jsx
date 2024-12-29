73dee189f9a66f99d9a988be97d000a6b514f782
RhynJke <myraj07.rjd@gmail.com>
2024-12-29 22:11:33 +0800

    new

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render( 
   <StrictMode>
-  <BrowserRouter basename="/Store-Management-System">
-    <App />
-  </BrowserRouter>
-</StrictMode>
+    <BrowserRouter basename="/Store-Management-System">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
