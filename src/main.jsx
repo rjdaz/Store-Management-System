0be96136670f3d27eab055df4b3de2ad7602304c
Dazzle <catc.daz07@gmail.com>
2025-01-10 21:37:47 +0800

    app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 const PUBLIC_URL = process.env.PUBLIC_URL || '/Store-Management-System';
 
