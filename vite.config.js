3346c080c4f33287fcd57e2a860339ddd5fb572a
Dazzle <catc.daz07@gmail.com>
2025-01-10 23:03:35 +0800

    new update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Store-Management-System/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx'],
