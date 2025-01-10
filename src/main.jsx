e079c39ec600a6044c4f51c3126d4545936f810f
Dazzle <catc.daz07@gmail.com>
2025-01-10 21:27:22 +0800

    src main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render( 
   <StrictMode>
-    <BrowserRouter basename="/Store-Management-System">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
